c0fe85a3a520c09425e9a649b75c50387a28d4f7
Ramprasad <ramprasad@Ramprasads-MacBook-Air.local>
2025-10-15 16:40:16 +0530

    prod commit

---
+++ terraform/environments/prod/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "shreyas-terraform-state-bucket-123456"
+    key            = "networking/vpc/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "git::https://github.com/Ramprasadvaral13/reusable-ami-module.git//networking?ref=main"
+
+  vpc_cidr   = var.vpc_cidr
+  route_cidr = var.route_cidr
+  subnets    = var.subnets
+}
+
+module "compute" {
+  source = "git::https://github.com/Ramprasadvaral13/reusable-ami-module.git//compute?ref=main"
+
+  name_prefix      = "prod"
+  vpc_id           = module.vpc.vpc_id
+  subnet_ids       = module.vpc.private_subnet_ids
+  instance_type    = var.instance_type
+  min_size         = var.min_size
+  max_size         = var.max_size
+  desired_capacity = var.desired_capacity
+  volume_size = var.volume_size
+}
+
---
+++ terraform/environments/prod/terraform.tfvars
@@ -0,0 +1,33 @@
+region     = "us-east-1"
+vpc_cidr   = "10.0.0.0/16"
+route_cidr = "0.0.0.0/0"
+
+subnets = {
+  "public-1" = {
+    cidr   = "10.0.1.0/24"
+    az     = "us-east-1a"
+    public = true
+  },
+  "public-2" = {
+    cidr   = "10.0.2.0/24"
+    az     = "us-east-1b"
+    public = true
+  },
+  "private-1" = {
+    cidr   = "10.0.3.0/24"
+    az     = "us-east-1a"
+    public = false
+  },
+  "private-2" = {
+    cidr   = "10.0.4.0/24"
+    az     = "us-east-1b"
+    public = false
+  }
+}
+
+instance_type    = "t2.large"
+min_size         = 3
+max_size         = 4
+desired_capacity = 2
+volume_size = 10
+
