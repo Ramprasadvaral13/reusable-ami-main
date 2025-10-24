70c4454c4b13926c42fed4217c398bac363cbc39
Ramprasadvaral13 <136104228+Ramprasadvaral13@users.noreply.github.com>
2025-10-24 19:42:44 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -38,6 +38,6 @@ module "compute" {
   max_size         = var.max_size
   desired_capacity = var.desired_capacity
   volume_size = var.volume_size
-  key_pair_name = var.key_pair_name
+  key_name = var.key_pair_name
 }
 
